a61c59684e8edbb95da3acce02525c8116676655
Dmitry Osobov <waffencode@gmail.com>
2025-04-25 10:28:20 +0300

    feat: update packages, implement struct for class page

--- src/pages/schedule/ClassPage.tsx
+++ src/pages/schedule/ClassPage.tsx
@@ -2,8 +2,17 @@ import AppPage from "@/components/AppPage";
 import { Button } from "@/components/ui/button";
 import ScheduleClass from "@/entities/domain/ScheduleClass";
 import { ApiContext } from "@/service/ApiProvider";
+import {
+	HStack,
+	NativeSelect,
+	Spinner,
+	Tabs,
+	Text,
+	VStack,
+} from "@chakra-ui/react";
 import { UUID } from "node:crypto";
 import React, { useContext, useEffect, useState } from "react";
+import { LuUsers } from "react-icons/lu";
 import { useNavigate, useParams } from "react-router-dom";
 
 const ClassPage = () => {
@@ -62,21 +71,118 @@ const ClassPage = () => {
 	return (
 		<AppPage title="Журнал занятия">
 			<>
-				<pre
-					style={{
-						whiteSpace: "pre-wrap",
-						wordWrap: "break-word",
-						background: "white",
-						padding: "1rem",
-						borderRadius: "4px",
-						marginTop: "1rem",
-					}}
-				>
-					{JSON.stringify(scheduleClass, null, 2)}
-				</pre>
-				<Button variant="surface" onClick={() => navigate("/schedule")}>
-					Назад
-				</Button>
+				{/*<pre*/}
+				{/*	style={{*/}
+				{/*		whiteSpace: "pre-wrap",*/}
+				{/*		wordWrap: "break-word",*/}
+				{/*		background: "white",*/}
+				{/*		padding: "1rem",*/}
+				{/*		borderRadius: "4px",*/}
+				{/*		marginTop: "1rem",*/}
+				{/*	}}*/}
+				{/*>*/}
+				{/*	{JSON.stringify(scheduleClass, null, 2)}*/}
+				{/*</pre>*/}
+				{scheduleClass === undefined ? (
+					<Spinner />
+				) : (
+					<>
+						<p>Учебные группы на этом занятии:</p>
+						<Tabs.Root>
+							<Tabs.List>
+								{scheduleClass.groups?.map((group) => (
+									<Tabs.Trigger
+										key={group.id}
+										value={group.id}
+									>
+										<LuUsers />
+										{group.name}
+									</Tabs.Trigger>
+								))}
+							</Tabs.List>
+							{scheduleClass.groups?.map((group) => (
+								<Tabs.Content key={group.id} value={group.id}>
+									<VStack gap={3} align="stretch" w="100%">
+										{group.students.map((student) => (
+											<HStack
+												key={student.id}
+												p={1}
+												bg="white"
+												borderRadius="md"
+												borderWidth="1px"
+												borderColor="gray.100"
+												justifyContent="space-between"
+												_hover={{ bg: "gray.50" }}
+											>
+												<Text fontWeight="medium">
+													{student.fullName}
+												</Text>
+
+												<HStack gap={4}>
+													<NativeSelect.Root
+														size="sm"
+														defaultValue={""}
+													>
+														<NativeSelect.Field>
+															<option value="П">
+																Присутствует
+															</option>
+															<option value="Б">
+																Болел(-а)
+															</option>
+															<option value="Н">
+																Уважительная
+																причина
+															</option>
+															<option value="О">
+																Отсутствовал(-а)
+															</option>
+														</NativeSelect.Field>
+														<NativeSelect.Indicator />
+													</NativeSelect.Root>
+
+													<HStack gap={2}>
+														{[1, 2, 3, 4, 5].map(
+															(grade) => (
+																<Button
+																	key={grade}
+																	size="sm"
+																	colorScheme={
+																		5 ===
+																		grade
+																			? "blue"
+																			: "gray"
+																	}
+																	variant={
+																		5 ===
+																		grade
+																			? "solid"
+																			: "outline"
+																	}
+																>
+																	{grade}
+																</Button>
+															),
+														)}
+													</HStack>
+												</HStack>
+											</HStack>
+										))}
+									</VStack>
+								</Tabs.Content>
+							))}
+						</Tabs.Root>
+					</>
+				)}
+
+				<HStack gap={4} mt={4}>
+					<Button
+						variant="surface"
+						onClick={() => navigate("/schedule")}
+					>
+						Назад
+					</Button>
+				</HStack>
 			</>
 		</AppPage>
 	);
