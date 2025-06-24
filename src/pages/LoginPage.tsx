f88ba4db29908afb221b3ddea690d5801c79db56
Dmitry Osobov <waffencode@gmail.com>
2025-06-24 23:56:03 +0300

    feat: add demo mode notification

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,12 +3,28 @@ import HeaderBar from "@/components/headerbar/HeaderBar";
 import { PasswordInput } from "@/components/ui/password-input";
 import { ApiContext } from "@/service/ApiProvider";
 import { UserContext } from "@/service/UserProvider";
-import { Alert, Button, Card, Center, HStack, Input, Link, Spinner, Text, VStack } from "@chakra-ui/react";
+import {
+	Alert,
+	Box,
+	Button,
+	Card,
+	Center,
+	Flex,
+	Heading,
+	HStack,
+	Input,
+	Link,
+	SimpleGrid,
+	Spinner,
+	Text,
+	VStack,
+} from "@chakra-ui/react";
 import axios from "axios";
 import CryptoJS from "crypto-js";
 import React, { useContext, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
 
 interface LoginFormData {
 	email: string;
@@ -61,8 +77,113 @@ const LoginPage: React.FC = () => {
 	return (
 		<>
 			<HeaderBar />
-			<Center h="60vh" px="10" maxW={isMobile ? "100%" : "40%"} w="100%" mx="auto">
-				<Card.Root p={1} rounded="md" boxShadow="md" w="90%" size="sm">
+			<Center mt={15} gap={2} h="60vh" px="10" maxW={isMobile ? "100%" : "90%"} w="100%" mx="auto">
+				{import.meta.env.VITE_DEMO_MODE_BANNER == "true" && (
+					<Box
+						p={6}
+						mt={15}
+						w="40%"
+						borderRadius="xl"
+						bgGradient="linear(to-r, blue.50, purple.50)"
+						borderWidth="1px"
+						borderColor="gray.100"
+						className="demo-advertisement-container"
+					>
+						<Flex direction="column" gap={3}>
+							<Heading as="h2" size="lg" color="blue.800" fontWeight="semibold" letterSpacing="tight">
+								Это демонстрационный клиент системы Университет!
+							</Heading>
+
+							<Text fontSize="md" color="gray.600">
+								Оцените все возможности платформы с тестовыми учетными записями:
+							</Text>
+
+							<SimpleGrid columns={{ base: 1, md: 2 }} gap={2} mt={2} color="black">
+								<Box
+									bg="white"
+									p={4}
+									borderRadius="lg"
+									shadow="sm"
+									borderLeft="4px solid"
+									borderColor="blue.400"
+								>
+									<Text fontWeight="medium" color="blue.700" mb={2}>
+										Администратор
+									</Text>
+									<Text fontSize="sm">
+										<b>Логин:</b> admin@example.com
+									</Text>
+									<Text fontSize="sm">
+										<b>Пароль:</b> admin
+									</Text>
+								</Box>
+
+								<Box
+									bg="white"
+									p={4}
+									borderRadius="lg"
+									shadow="sm"
+									borderLeft="4px solid"
+									borderColor="purple.400"
+								>
+									<Text fontWeight="medium" color="purple.700" mb={2}>
+										Студент
+									</Text>
+									<Text fontSize="sm">
+										<b>Логин:</b> student@example.com
+									</Text>
+									<Text fontSize="sm">
+										<b>Пароль:</b> student
+									</Text>
+								</Box>
+
+								<Box
+									bg="white"
+									p={4}
+									borderRadius="lg"
+									shadow="sm"
+									borderLeft="4px solid"
+									borderColor="teal.400"
+								>
+									<Text fontWeight="medium" color="teal.700" mb={2}>
+										Методист
+									</Text>
+									<Text fontSize="sm">
+										<b>Логин:</b> manager@example.com
+									</Text>
+									<Text fontSize="sm">
+										<b>Пароль:</b> manager
+									</Text>
+								</Box>
+
+								<Box
+									bg="white"
+									p={4}
+									borderRadius="lg"
+									shadow="sm"
+									borderLeft="4px solid"
+									borderColor="orange.400"
+								>
+									<Text fontWeight="medium" color="orange.700" mb={2}>
+										Преподаватель
+									</Text>
+									<Text fontSize="sm">
+										<b>Логин:</b> teacher@example.com
+									</Text>
+									<Text fontSize="sm">
+										<b>Пароль:</b> teacher
+									</Text>
+								</Box>
+							</SimpleGrid>
+
+							<Text mt={3} fontSize="sm" color="gray.500" fontStyle="italic">
+								После входа вы сможете протестировать функционал, соответствующий роли пользователя.
+							</Text>
+						</Flex>
+					</Box>
+				)}
+
+				<Card.Root p={1} rounded="md" boxShadow="md" w="30%" size="sm">
 					<Card.Header>
 						<Card.Title>Авторизация</Card.Title>
 					</Card.Header>
